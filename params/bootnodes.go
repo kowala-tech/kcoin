ceff0db422b770ee34a365d631245d2a69195d39
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-02-02 10:27:57 +0000

    params: rebase - second review

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -1,24 +1,13 @@
 package params
 
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
-// the main Ethereum network.
-var MainnetBootnodes = []string{
-	// Kowala Foundation Go Bootnodes
-	"enode://a979fb575495b8d6db44f750317d0f4622bf4c2aa3365d6af7c284339968eef29b69ad0dce72a4d8db5ebb4968de0e3bec910127f134779fbcb0cb6d3331163c@52.16.188.185:22334", // IE
-
-}
+// the main Kowala network.
+var MainnetBootnodes = []string{}
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // test network.
-var TestnetBootnodes = []string{
-	"enode://6ce05930c72abc632c58e2e4324f7c7ea478cec0ed4fa2528982cf34483094e9cbc9216e7aa349691242576d552a2a56aaeae426c5303ded677ce455ba1acd9d@13.84.180.240:22334", // US-TX
-
-}
+var TestnetBootnodes = []string{}
 
 // DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
 // experimental RLPx v5 topic-discovery network.
-var DiscoveryV5Bootnodes = []string{
-	"enode://0cc5f5ffb5d9098c8b8c62325f3797f56509bff942704687b6530992ac706e2cb946b90a34f1f19548cd3c7baccbcaea354531e5983c7d1bc0dee16ce4b6440b@40.118.3.223:30305",
-	"enode://1c7a64d76c0334b0418c004af2f67c50e36a3be60b5e4790bdac0439d21603469a85fad36f2473c9a80eb043ae60936df905fa28f1ff614c3e5dc34f15dcd2dc@40.118.3.223:30308",
-	"enode://85c85d7143ae8bb96924f2b54f1b3e70d8c4d367af305325d30a61385a432f247d2c75c45c6b4a60335060d072d7f5b35dd1d4c45f76941f62a4f83b6e75daaf@40.118.3.223:30309",
-}
+var DiscoveryV5Bootnodes = []string{}
--- params/protocol_params.go
+++ params/protocol_params.go
@@ -15,23 +15,16 @@ const (
 	SstoreSetGas          uint64 = 20000 // Once per SLOAD operation.
 	LogDataGas            uint64 = 8     // Per byte in a LOG* operation's data.
 	CallStipend           uint64 = 2300  // Free gas given at beginning of call.
-	EcrecoverGas          uint64 = 3000  //
-	Sha256WordGas         uint64 = 12    //
 
 	Sha3Gas          uint64 = 30    // Once per SHA3 operation.
-	Sha256Gas        uint64 = 60    //
-	IdentityWordGas  uint64 = 3     //
 	Sha3WordGas      uint64 = 6     // Once per word of the SHA3 operation's data.
 	SstoreResetGas   uint64 = 5000  // Once per SSTORE operation if the zeroness changes from zero.
 	SstoreClearGas   uint64 = 5000  // Once per SSTORE operation if the zeroness doesn't change.
 	SstoreRefundGas  uint64 = 15000 // Once per SSTORE operation if the zeroness changes to zero.
 	JumpdestGas      uint64 = 1     // Refunded gas, once per SSTORE operation if the zeroness changes to zero.
-	IdentityGas      uint64 = 15    //
 	EpochDuration    uint64 = 30000 // Duration between proof-of-work epochs.
 	CallGas          uint64 = 40    // Once per CALL operation & message call transaction.
 	CreateDataGas    uint64 = 200   //
-	Ripemd160Gas     uint64 = 600   //
-	Ripemd160WordGas uint64 = 120   //
 	CallCreateDepth  uint64 = 1024  // Maximum depth of call/create stack.
 	ExpGas           uint64 = 10    // Once per EXP instruction
 	LogGas           uint64 = 375   // Per LOG* operation.
@@ -46,6 +39,21 @@ const (
 
 	MaxCodeSize = 24576
 
+	// Precompiled contract gas prices
+
+	EcrecoverGas            uint64 = 3000   // Elliptic curve sender recovery gas price
+	Sha256BaseGas           uint64 = 60     // Base price for a SHA256 operation
+	Sha256PerWordGas        uint64 = 12     // Per-word price for a SHA256 operation
+	Ripemd160BaseGas        uint64 = 600    // Base price for a RIPEMD160 operation
+	Ripemd160PerWordGas     uint64 = 120    // Per-word price for a RIPEMD160 operation
+	IdentityBaseGas         uint64 = 15     // Base price for a data copy operation
+	IdentityPerWordGas      uint64 = 3      // Per-work price for a data copy operation
+	ModExpQuadCoeffDiv      uint64 = 20     // Divisor for the quadratic particle of the big int modular exponentiation
+	Bn256AddGas             uint64 = 500    // Gas needed for an elliptic curve addition
+	Bn256ScalarMulGas       uint64 = 40000  // Gas needed for an elliptic curve scalar multiplication
+	Bn256PairingBaseGas     uint64 = 100000 // Base price for an elliptic curve pairing check
+	Bn256PairingPerPointGas uint64 = 80000  // Per-point price for an elliptic curve pairing check
+
 	// Proof of Stake - timeouts
 	ProposeDuration        uint64 = 10000
 	ProposeDeltaDuration   uint64 = 5000
@@ -53,10 +61,7 @@ const (
 	PreVoteDeltaDuration   uint64 = 5000
 	PreCommitDuration      uint64 = 10000
 	PreCommitDeltaDuration uint64 = 5000
-
-	// @NOTE (rgeraldes) - this duration can be used to change the times
-	// between blocks
-	SyncDuration uint64 = 15000
+	SyncDuration           uint64 = 15000
 )
 
 var (
