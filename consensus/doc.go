34baafb9e968521780f7194b537417589112c72b
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-02-21 16:42:31 +0000

    consensus/doc: reviewed format

--- consensus/doc.go
+++ consensus/doc.go
@@ -1,17 +1,24 @@
 /*
 
-Package consensus implements consensus protocols - the implementations satisfy the Engine Interface (consensus.go)
-The consensus protocols are responsible for operations such as checking whether a header conforms to the consensus rules,
-running any post-transaction state modifications (Ex: Block rewards) and assembling the final block.
-The kowala dev/research team is currently focused on Proof-of-Stake protocols.
+Package consensus implements consensus protocols - the implementations satisfy
+the Engine Interface (consensus.go)
 
-We're actively improving the protocols - the codebase supports the following protocols at the moment:
+The consensus protocols are responsible for operations such as checking whether
+a header conforms to the consensus rules, running any post-transaction state
+modifications (Ex: Block rewards) and assembling the final block. The kowala
+dev/research team is currently focused on Proof-of-Stake protocols.
 
-Proof-of-Stake:
+We're actively improving the protocols - the codebase supports the following
+protocols at the moment:
 
-* Tendermint Consensus Engine - Tendermint is a mostly asynchronous BFT consensus protocol. Participants in the protocol are called
-validators and they take turns on proposing blocks of transactions and voting on them. Assuming that less than one third of the validators
-are Byzantine, Tendermint guarantees that safety will never be violated. Block finality(every block is final) in Tendermint can be achieved in 1 second.
+Proof-of-Stake
+
+* Tendermint Consensus Engine - Tendermint is a mostly asynchronous BFT
+consensus protocol. Participants in the protocol are called validators and they
+take turns on proposing blocks of transactions and voting on them. Assuming that
+less than one third of the validators are Byzantine, Tendermint guarantees that
+safety will never be violated. Block finality(every block is final) in
+Tendermint can be achieved in 1 second.
 
 */
 
