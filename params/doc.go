03be00a815626ada02970159762f207e4faf57c7
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-02-26 16:35:37 +0000

    params/doc: added initial version

---
+++ params/doc.go
@@ -0,0 +1,17 @@
+/*
+
+The params package implements system variables
+
+| File               | Description                                   |
+|--------------------|-----------------------------------------------|
+| bootnodes.go       | go-to bootnodes per network                   |
+| config.go          | default chain config per network              |
+| denomination.go    | multipliers for kowala denominations          |
+| gas_table.go       | gas prices for the Andromeda phase            |
+| network_params.go  | network params                                |
+| protocol_params.go | protocol params (example: consensus timeouts) |
+| version.go         | version metadata                              |
+
+*/
+
+package params
