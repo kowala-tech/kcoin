f87be11e38f8c9729034ce2e6bcb2f267594b9d2
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-02-27 15:22:59 +0000

    common/doc: initial version

---
+++ common/doc.go
@@ -0,0 +1,9 @@
+/*
+
+Package common is an utility package that covers relevant elements of the
+blockchain such as the Address and Hash type, hexadecimal utils and specific
+formats.
+
+*/
+
+package common
