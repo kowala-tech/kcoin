bb460a34393eec2b06ea15380fb066eeb36e9402
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-02-27 15:14:55 +0000

    p2p/doc: added initial version

---
+++ p2p/doc.go
@@ -0,0 +1,32 @@
+/*
+Package p2p implements the Kowala p2p network protocols
+
+Peer Discovery
+
+The network has nodes that are assumed to be always available - bootstrap nodes.
+The bootstrap nodes maintain a list of all nodes that connected to them in
+period of time. When a node connects to the Kowala network, they first connect
+to the bootstrap nodes which share the list of peers.
+
+The list of hardcoded bootstrap nodes per network can be found under
+params/bootnodes.go
+
+Peer Selection
+
+Kowala uses a kademlia-like system to discover further peers.
+The current codebase supports two peer discovery mechanisms: v4 and v5.
+The v5 version enables discovery based on topics - protocols that the
+node has enabled (matching protocols).
+
+Pre-configured Connections
+
+The p2p server supports two types of pre-configured connections:
+
+* Static nodes - pre-configured connections which are always
+maintained and re-connected on disconnects.
+
+* Trusted nodes - pre-configured connections which are always
+allowed to connect, even above the peer limit.
+*/
+
+package p2p
