945d51fcb731b54925bb4678e3c175e4d645a921
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-02-27 15:52:04 +0000

    common/doc: added package info

--- common/doc.go
+++ common/doc.go
@@ -1,8 +1,17 @@
 /*
 
 Package common is an utility package that covers relevant elements of the
-blockchain such as the Address and Hash type, hexadecimal utils and specific
-formats.
+blockchain such as the Kowala's account address type, hexadecimal utils (etc).
+
+Packages
+
+| Package  | Description                                                  |
+|----------|--------------------------------------------------------------|
+| bitutil  | implements fast bitwise operations                           |
+| compiler | wraps the Solidity compiler executable (solc)                |
+| hexutil  | implements hex encoding with 0x prefix (used by the RPC API) |
+| math     | provides integer math utilities                              |
+| mclock   | wrapper for a monotonic clock source                         |
 
 */
 
