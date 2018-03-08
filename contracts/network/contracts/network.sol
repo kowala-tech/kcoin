efe4eb4e11fa700d41900e6417f6af62109927f0
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-03-08 18:17:54 +0000

    contracts/network: latest version 8/3

--- contracts/network/contracts/network.sol
+++ contracts/network/contracts/network.sol
@@ -4,7 +4,7 @@ import "./ownable.sol";
 
 contract Network is Ownable {
 
-    // @NOTE (rgeraldes) - to be confirmed by Hélio
+    // @NOTE (rgeraldes) - to be confirmed by Hélio Rosa
     /*
     // Total supply of wei. Must be updated every block and initialized to the correct value.
     uint256 public totalSupplyWei = 1 ether;
@@ -14,18 +14,84 @@ contract Network is Ownable {
     uint256 public lastPrice = 0;
     */
 
-    // minimum deposit that a candidate has to do in order to 
+    // base deposit represents the deposit that a candidate has to do in order to 
     // secure a place in the elections (if there are positions available)
-    uint public minDeposit;       
+    uint public baseDeposit;       
     
-    // minimum deposit hard limits (safety)
-    uint public minDepositUpperBound;
-    uint public minDepositLowerBound;
+    // base deposit hard limits (safety)
+    uint public baseDepositUpperBound;
+    uint public baseDepositLowerBound;
 
-    // @NOTE (rgeraldes) - this field is used to know the address of the 
-    // genesis validator (does not need to make a deposit)
-    address public genesis;
+    // onlyWithinMinDepositBounds requires the new deposit value to be within valid bounds
+    modifier onlyWithinBaseDepositBounds(uint deposit) {
+        require(deposit >= baseDepositLowerBound && deposit <= baseDepositUpperBound);
+        _;
+    }
 
+    // setBaseDepositLowerBound sets the lower bound of the minimum deposit operation
+    function setBaseDepositLowerBound(uint min) public onlyOwner {
+        require(min <= baseDepositUpperBound);
+        baseDepositLowerBound = min;
+    }
+
+    // setBaseDepositUpperBound sets the upper bound of the minimum deposit operation
+    function setBaseDepositUpperBound(uint max) public onlyOwner {
+        require(max >= baseDepositLowerBound);
+        baseDepositUpperBound = max;
+    }
+
+    // setBaseDeposit sets the minimum deposit accepted by the network to join the consensus
+    // elections.
+    function setBaseDeposit(uint deposit) public onlyOwner onlyWithinBaseDepositBounds(deposit) {
+        baseDeposit = deposit;
+    }
+
+
+    // maxValidators represents the maximum number of validators allowed
+    // in a consensus election at once
+    uint public maxValidators;
+
+    // maxValidators hard limits (safety)
+    uint public maxValidatorsUpperBound;
+    uint public maxValidatorsLowerBound;
+
+    // onlyWithinMaxValidatorBounds requires the new value to be within valid bounds
+    modifier onlyWithinMaxValidatorBounds(uint max) {
+        require(max >= maxValidatorsLowerBound && max <= maxValidatorsUpperBound);
+        _;
+    }
+
+    function setMaxValidatorsLowerBound(uint min) public onlyOwner {
+        require(min <= maxValidatorsUpperBound);
+        maxValidatorsLowerBound = min;
+    }
+
+    function setMaxValidatorsUpperBound(uint max) public onlyOwner {
+        require(max >= maxValidatorsLowerBound);
+        maxValidatorsUpperBound = max;
+    }
+
+    // validatorIndex contains the validator code ordered by the biggest deposit to
+    // the smallest deposit.
+    address[] validatorIndex;
+
+    // availability returns the number of positions for validator available
+    function availability() public view returns (uint available) {
+        return maxValidators - validatorIndex.length;
+    }
+
+    function setMaxValidators(uint max) public onlyOwner onlyWithinMaxValidatorBounds(max) { 
+        if (availability() == 0) {
+            uint toRemove = (maxValidators - max);
+            for (uint i = 0; i < toRemove; i++) {
+                _deregisterLastValidator();
+            }
+        }
+        maxValidators = max;
+    }
+
+    
+    // Deposit represents the collateral - staked tokens
     struct Deposit {
         uint amount;
         uint releasedAt;
@@ -43,29 +109,79 @@ contract Network is Ownable {
         Deposit[] deposits; 
     }
 
-    // validators contains the information of the validators
     mapping (address => Validator) private validators;
 
-    // validatorIndex is used in order to have an ordered insert
-    address[] private validatorIndex; 
-
-    // validatorsChecksum is a representation of the current set of validators.
+    // @NOTE (rgeraldes) - event filtering is a possibility in the future (issue #140)
+    // validatorsChecksum is a representation of the current set of validators
     bytes32 public validatorsChecksum;
 
-    // maximum number of validators at one time
-    uint public maxValidators;
+    function _insertValidator(address code, uint deposit) private {
+        validators[code].deposits.push(Deposit({amount:deposit, releasedAt: 0}));
+        validators[code].isValidator = true;
+        
+        // ordered insert based on the deposit value
+        validators[code].index = validatorIndex.push(code) - 1;
+    }
 
-    // maxValidators hard limits (safety)
-    uint public maxValidatorsUpperBound;
-    uint public maxValidatorsLowerBound;
+    function _registerCandidate(address code, uint deposit) private {
+        _insertValidator(code, deposit);
+    }
+
+    // genesis stores the registration code of the genesis validator
+    address public genesis;
 
     // unbondingPeriod is a predetermined period of time that coins remain locked
     // starting from the moment a validator leaves the consensus elections
     uint public unbondingPeriod;
 
+
+    function Network(uint _baseDeposit, address _genesis, uint _maxValidators, uint _unbondingPeriod) public payable {
+        require(msg.value >= _baseDeposit);
+        require(_maxValidators >= 1);
+        require(_unbondingPeriod >= 0);
+
+
+        baseDeposit = _baseDeposit;
+        maxValidators = _maxValidators;
+        genesis = _genesis;
+        unbondingPeriod = _unbondingPeriod;
+
+        baseDepositLowerBound = baseDeposit / 2;
+        baseDepositUpperBound = baseDeposit * 2;
+        maxValidatorsLowerBound = maxValidators / 2;
+        maxValidatorsUpperBound = maxValidators * 2;
+
+        _registerCandidate(_genesis, msg.value);
+    }
+
+
+
+
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+
     // onlyWithMinDeposit requires a minimum deposit to proceed
     modifier onlyWithMinDeposit {
-        require(msg.value >= minDeposit);
+        require(msg.value >= getMinimumDeposit());
         _;
     } 
 
@@ -75,49 +191,29 @@ contract Network is Ownable {
         _;
     }
 
-    // onlyWithinMaxValidatorBounds requires the new value to be within valid bounds
-    modifier onlyWithinMaxValidatorBounds(uint max) {
-        require(max >= maxValidatorsLowerBound && max <= maxValidatorsUpperBound);
-        _;
+    
+
+    
+
+    // getMinimumDeposit returns the base deposit if there are positions available or
+    // the current smallest deposit required if there aren't positions availabe.
+    function getMinimumDeposit() public view returns (uint deposit) {
+        // there are no positions available
+        if (validatorIndex.length == maxValidators) {
+            // value needs to be bigger than the smallest deposit
+            // the smallest deposit corresponds to the current deposit 
+            // of the last validator in validatorIndex array
+            Validator lastValidator = validators[validatorIndex.length];               
+            uint smallestDeposit = lastValidator.Deposits[lastValidator.Deposits.length - 1];
+            deposit = smallestDeposit + 1; 
+        } else {
+            deposit = baseDeposit;
+        }
     }
 
-    // onlyWithinMinDepositBounds requires the new value to be within valid bounds
-    modifier onlyWithinMinDepositBounds(uint deposit) {
-        require(deposit >= minDepositLowerBound && deposit <= minDepositUpperBound);
-        _;
-    }
+    
 
-
-    // setMinDepositUpperBound sets the upper bound of the minimum deposit operation
-    function setMinDepositUpperBound(uint max) public onlyOwner {
-        require(max >= minDepositLowerBound);
-        minDepositUpperBound = max;
-    }
-
-    // setMinDepositLowerBound sets the lower bound of the minimum deposit operation
-    function setMinDepositLowerBound(uint min) public onlyOwner {
-        require(min <= minDepositUpperBound);
-        minDepositLowerBound = min;
-    }
-
-    // setMinDeposit sets the minimum deposit accepted by the network to join the consensus
-    // elections.
-    function setMinDeposit(uint deposit) public onlyOwner onlyWithinMinDepositBounds(deposit) {
-        minDeposit = deposit;
-    }
-
-    function Network(uint _minDeposit, address _genesis, uint _maxValidators, uint _unbondingPeriod) public {
-        minDeposit = _minDeposit;
-        minDepositLowerBound = minDeposit / 2;
-        minDepositUpperBound = minDeposit * 2;
-        maxValidators = _maxValidators;
-        maxValidatorsLowerBound = maxValidators / 2;
-        maxValidatorsUpperBound = maxValidators * 2;
-        genesis = _genesis;
-        unbondingPeriod = _unbondingPeriod;
-        // @TODO (register the genesis validator)
-        //  _registerValidator(investor1, investment1);
-    }
+    
 
     function isGenesisValidator(address code) public view returns (bool isIndeed) {
         return code == genesis;
@@ -131,62 +227,22 @@ contract Network is Ownable {
         return validatorIndex.length;
     }
 
-    // availability returns the number of positions available
-    function availability() public view returns (uint available) {
-        return maxValidators - validatorIndex.length;
-    }
+    
 
-    function setMaxValidatorsUpperBound(uint max) public onlyOwner {
-        require(max >= maxValidatorsLowerBound);
-        maxValidatorsUpperBound = max;
-    }
+    
 
-    function setMaxValidatorsLowerBound(uint min) public onlyOwner {
-        require(min <= maxValidatorsUpperBound);
-        maxValidatorsLowerBound = min;
-    }
-
-    function setMaxValidators(uint max) public onlyOwner onlyWithinMaxValidatorBounds(max) { 
-        if (max < maxValidators) {
-            // @NOTE (rgeraldes) - if the new max if smaller than the current one
-            // we will have to remove (maxValidators - max) validators from the election if 
-            // all the validator positions are occupied. We start by removing the validators
-            // which appear in the last positions as it represents the validators with 
-            // the smallest stake at play. 
-            uint toRemove = (maxValidators - max);
-            for (uint i = 0; i < toRemove; i++) {
-                _deregisterLastValidator();
-            }
-        }
-        maxValidators = max;
-    }
-
+    
     // deposit increments the deposit of a validator/registers a new candidate
-    function deposit() public payable {
-        if (isValidator(msg.sender)) {
-            _updateDepositAmount();
-        } else {
-            _newCandidate();
+    function deposit() public payable onlyWithMinDeposit  {
+        if (validatorIndex.length == maxValidators) {
+            _deregisterValidator();
         }
+        _registerCandidate(msg.sender, msg.value);
     }
 
     function _updateDepositAmount() private {
         Deposit deposit = validators[msg.sender].collaterals[validatorIndex.length - 1];
         deposit.amount += msg.value;
-    }
-
-    function _newCandidate() private onlyWithMinDeposit {
-        // there are no positions available
-        if (validatorIndex.length == maxValidators) {
-            // value needs to be bigger than the smallest deposit
-            // the smallest deposit corresponds to the current deposit 
-            // of the last validator in validatorIndex array
-            Validator lastValidator = validators[validatorIndex.length];               
-            uint smallestDeposit = lastValidator.Deposits[lastValidator.Deposits.length - 1];
-            require(msg.value > smallestDeposit);
-            _deregisterValidator();
-        }
-        _registerCandidate(msg.sender, msg.value);
     }
 
 
@@ -238,10 +294,7 @@ contract Network is Ownable {
         _updateVotersChecksum();
     }   
 
-    function _registerCandidate(address account, uint deposit) {
-        _insertValidator(account, deposit);
-        _updateVotersChecksum();
-    }
+    
 
     function _setDepositReleaseDate(address account) private {
         // @NOTE (rgeraldes) - the current active collateral is the last one.
@@ -263,12 +316,7 @@ contract Network is Ownable {
         validators[account].isVoter = false;
     }
 
-    // _insertVoter adds a new voter record to the voterIndex array and voters map
-    function _insertValidator(address account, uint deposit) private {
-        voters[addr].collateral.push();
-        voters[addr].index = voterIndex.push(addr) - 1;
-        voters[addr].isVoter = true;
-    }
+    
     
     /*
     function remove(uint index)  returns(uint[]) {
