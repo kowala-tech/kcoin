429ad2d52e05986b3d94b7ece522e6e149bb7411
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-03-21 17:05:33 +0000

    contracts/network: added initial version of the contracts package

---
+++ contracts/doc.go
@@ -0,0 +1,9 @@
+/*
+Package contracts implements the core contracts
+
+Contract Testing
+
+// @NOTE (rgeraldes) - fund the owner account with enough balance to
+	// deploy the contract. The current value is more than enough.
+
+*/
