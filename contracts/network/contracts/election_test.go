2b6d30e5035f100d8c98f1e60216a4bcac7e4eaf
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-03-21 20:00:30 +0000

    contracts/network: added first batch of tests - election contract

--- contracts/network/contracts/election_test.go
+++ contracts/network/contracts/election_test.go
@@ -1,15 +1,14 @@
 package contracts
 
 import (
-	"context"
 	"crypto/ecdsa"
 	"errors"
-	"fmt"
 	"math/big"
 	"testing"
 
 	"github.com/kowala-tech/kUSD/accounts/abi/bind"
 	"github.com/kowala-tech/kUSD/accounts/abi/bind/backends"
+	"github.com/kowala-tech/kUSD/common"
 	"github.com/kowala-tech/kUSD/core"
 	"github.com/kowala-tech/kUSD/crypto"
 	"github.com/kowala-tech/kUSD/params"
@@ -78,7 +77,7 @@ func (suite *ElectionContractSuite) NewSimulatedBackend() *backends.SimulatedBac
 
 func (suite *ElectionContractSuite) DeployElectionContract(baseDeposit, maxValidators, unbondingPeriod *big.Int) error {
 	opts := bind.NewKeyedTransactor(suite.owner)
-	addr, _, contract, err := DeployElectionContract(opts, suite.backend, baseDeposit, maxValidators, unbondingPeriod, crypto.PubkeyToAddress(suite.genesisValidator.PublicKey))
+	_, _, contract, err := DeployElectionContract(opts, suite.backend, baseDeposit, maxValidators, unbondingPeriod, crypto.PubkeyToAddress(suite.genesisValidator.PublicKey))
 	if err != nil {
 		return err
 	}
@@ -87,14 +86,12 @@ func (suite *ElectionContractSuite) DeployElectionContract(baseDeposit, maxValid
 	// NOTE (rgeraldes) - add balance to cover the base deposit
 	// for the genesis validator. Eventually this could change
 	// as soon as the token contracts are completed.
-	//suite.backend.SendTransaction(context.TODO(), types.NewTransaction(uint64(0), addr, suite.baseDeposit, nil, nil, []byte("")))
-	suite.backend.Commit()
-
-	balance, err := suite.backend.BalanceAt(context.TODO(), addr, suite.backend.CurrentBlock().Number())
+	opts.Value = suite.baseDeposit
+	_, err = contract.ElectionContractTransactor.contract.Transfer(opts)
 	if err != nil {
 		return err
 	}
-	fmt.Println(balance)
+	suite.backend.Commit()
 
 	return nil
 }
@@ -126,4 +123,143 @@ func (suite *ElectionContractSuite) TestDeployElectionContract() {
 	genesisValidator, err := suite.contract.GenesisValidator(&bind.CallOpts{})
 	req.NoError(err)
 	req.Equal(crypto.PubkeyToAddress(suite.genesisValidator.PublicKey), genesisValidator)
+}
+
+func (suite *ElectionContractSuite) TestDeployElectionContract_MaxValidatorsEqualZero() {
+	req := suite.Require()
+
+	maxValidators := common.Big0
+	req.Equal(errTransactionFailed, suite.DeployElectionContract(suite.baseDeposit, maxValidators, suite.unbondingPeriod))
+}
+
+func (suite *ElectionContractSuite) TestGetOwner() {
+	req := suite.Require()
+
+	latestOwner, err := suite.contract.GetOwner(&bind.CallOpts{})
+	req.NoError(err)
+	req.Equal(crypto.PubkeyToAddress(suite.owner.PublicKey), latestOwner)
+}
+
+func (suite *ElectionContractSuite) TestTransferOwnership_NotOwner() {
+	req := suite.Require()
+
+	// future owner
+	newOwnerPK, err := crypto.GenerateKey()
+	req.NoError(err)
+	newOwnerAddr := crypto.PubkeyToAddress(newOwnerPK.PublicKey)
+	_, err = suite.contract.TransferOwnership(bind.NewKeyedTransactor(suite.randomUser), newOwnerAddr)
+	req.Equal(errTransactionFailed, err)
+}
+
+func (suite *ElectionContractSuite) TestTransferOwnership_Owner() {
+	req := suite.Require()
+
+	newOwner := getAddress(suite.randomUser)
+	_, err := suite.contract.TransferOwnership(bind.NewKeyedTransactor(suite.owner), newOwner)
+	req.NoError(err)
+
+	suite.backend.Commit()
+
+	latestOwner, err := suite.contract.GetOwner(&bind.CallOpts{})
+	req.NoError(err)
+	req.Equal(newOwner, latestOwner)
+}
+
+func (suite *ElectionContractSuite) TestIsGenesis() {
+	req := suite.Require()
+
+	testCases := []struct {
+		input  common.Address
+		output bool
+	}{
+		{
+			input:  getAddress(suite.genesisValidator),
+			output: true,
+		},
+		{
+			input:  getAddress(suite.randomUser),
+			output: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		isGenesis, err := suite.contract.IsGenesisValidator(&bind.CallOpts{}, tc.input)
+		req.NoError(err)
+		req.Equal(tc.output, isGenesis)
+	}
+}
+
+func (suite *ElectionContractSuite) TestIsValidator() {
+	req := suite.Require()
+
+	testCases := []struct {
+		input  common.Address
+		output bool
+	}{
+		{
+			input:  getAddress(suite.genesisValidator),
+			output: true,
+		},
+		{
+			input:  getAddress(suite.randomUser),
+			output: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		isValidator, err := suite.contract.IsValidator(&bind.CallOpts{}, tc.input)
+		req.NoError(err)
+		req.Equal(tc.output, isValidator)
+	}
+}
+
+func (suite *ElectionContractSuite) TestGetMinimumDeposit_ElectionFull() {
+	req := suite.Require()
+
+	// leave a position available for the genesis validator - max validators = 1
+	maxValidators := new(big.Int).SetUint64(1)
+	suite.DeployElectionContract(suite.baseDeposit, maxValidators, suite.unbondingPeriod)
+
+	minDeposit, err := suite.contract.GetMinimumDeposit(&bind.CallOpts{})
+	req.NoError(err)
+	// min deposit should be greater (+ 1) than the smallest stake
+	// at play which is equal to the base deposit (genesis validator)
+	req.Equal((new(big.Int).Add(suite.baseDeposit, common.Big1)), minDeposit)
+}
+
+func (suite *ElectionContractSuite) TestGetMinimumDeposit_ElectionNotFull() {
+	// by default the contract has one validator (genesis) and 99 (100 - 1)
+	// positions available
+	req := suite.Require()
+
+	minDeposit, err := suite.contract.GetMinimumDeposit(&bind.CallOpts{})
+	req.NoError(err)
+	// min deposit should be equal to the base deposit since
+	// there are positions available
+	req.Equal(suite.baseDeposit, minDeposit)
+}
+
+func (suite *ElectionContractSuite) TestSetBaseDeposit_NotOwner() {
+	req := suite.Require()
+
+	_, err := suite.contract.SetBaseDeposit(bind.NewKeyedTransactor(suite.randomUser), common.Big0)
+	req.Equal(errTransactionFailed, err)
+}
+
+func (suite *ElectionContractSuite) TestSetBaseDeposit_Owner() {
+	req := suite.Require()
+
+	deposit := new(big.Int).Add(suite.baseDeposit, common.Big1)
+	_, err := suite.contract.SetBaseDeposit(bind.NewKeyedTransactor(suite.owner), deposit)
+	req.NoError(err)
+
+	suite.backend.Commit()
+
+	latestDeposit, err := suite.contract.BaseDeposit(&bind.CallOpts{})
+	req.NoError(err)
+	req.Equal(deposit, latestDeposit)
+}
+
+func getAddress(privateKey *ecdsa.PrivateKey) common.Address {
+	return crypto.PubkeyToAddress(privateKey.PublicKey)
 }
