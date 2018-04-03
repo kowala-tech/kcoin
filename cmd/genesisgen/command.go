5cdd0ffaae3b0a3618aa51c73de30ee94dab3729
Jonathan Gimeno <jgimeno@gmail.com>
2018-04-03 11:43:02 +0200

    Add validators to the command

--- cmd/genesisgen/command.go
+++ cmd/genesisgen/command.go
@@ -8,15 +8,21 @@ import (
 	"math/big"
 	"strings"
 	"github.com/pkg/errors"
+	"github.com/kowala-tech/kcoin/common"
 )
 
 var (
 	ErrEmptyMaxNumValidators = errors.New("max number of validators is mandatory")
+	ErrEmptyUnbondingPeriod = errors.New("unbonding period in days is mandatory")
+	ErrEmptyWalletAddressValidator = errors.New("Wallet address of genesis validator is mandatory")
+	ErrInvalidWalletAddressValidator = errors.New("unbonding period in days is mandatory")
 )
 
 type GenerateGenesisCommand struct {
 	network string
 	maxNumValidators string
+	unbondingPeriod string
+	walletAddressGenesisValidator string
 }
 
 type GenerateGenesisCommandHandler struct {
@@ -33,6 +39,16 @@ func (h *GenerateGenesisCommandHandler) Handle(command GenerateGenesisCommand) e
 		return err
 	}
 
+	unbondingPeriod, err := h.getUnbondingPeriod(command.unbondingPeriod)
+	if err != nil {
+		return err
+	}
+
+	walletAddressGenValidator, err := h.getWalletAddressGenesisValidator(command.walletAddressGenesisValidator)
+	if err != nil {
+		return err
+	}
+
 	genesis := &core.Genesis{
 		Timestamp: uint64(time.Now().Unix()),
 		GasLimit:  4700000,
@@ -43,16 +59,43 @@ func (h *GenerateGenesisCommandHandler) Handle(command GenerateGenesisCommand) e
 	fmt.Printf("%v\n", network)
 	fmt.Printf("%v\n", genesis)
 	fmt.Printf("%v\n", maxNumValidators)
+	fmt.Printf("%v\n", unbondingPeriod)
+	fmt.Printf("%v\n", walletAddressGenValidator)
 
 	return nil
 }
 
 func (h *GenerateGenesisCommandHandler) getMaxNumValidators(s string) (*big.Int, error) {
+	var numValidators *big.Int
+
 	if s = strings.TrimSpace(s); s == "" {
 		return nil, ErrEmptyMaxNumValidators
 	}
 
-
+	return numValidators, nil
+}
+func (handler *GenerateGenesisCommandHandler) getUnbondingPeriod(uP string) (*big.Int, error) {
+	if text := strings.TrimSpace(uP); text == "" {
+		return nil, ErrEmptyUnbondingPeriod
+	}
 
 	return nil, nil
 }
+func (handler *GenerateGenesisCommandHandler) getWalletAddressGenesisValidator(wA string) (*common.Address, error) {
+	stringAddr := wA
+
+	if text := strings.TrimSpace(wA); text == "" {
+		return nil, ErrEmptyWalletAddressValidator
+	}
+
+	if strings.HasPrefix(stringAddr, "0x") {
+		stringAddr = strings.TrimPrefix(stringAddr, "0x")
+	}
+
+	if len(stringAddr) != 40 {
+		return nil, ErrInvalidWalletAddressValidator
+	}
+
+	return nil, nil
+}
+
--- cmd/genesisgen/command_test.go
+++ cmd/genesisgen/command_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestItFailsWhenRunningHandlerWithInvalidCommandValues(t *testing.T) {
 	tests := []struct{
@@ -16,22 +18,52 @@ func TestItFailsWhenRunningHandlerWithInvalidCommandValues(t *testing.T) {
 			ExpectedError: ErrInvalidNetwork,
 		},
 		{
-			TestName: "Empty max number of validators.",
+			TestName: "Empty max number of validators",
 			InvalidCommand: GenerateGenesisCommand{
 				network: "test",
 				maxNumValidators: "",
 			},
 			ExpectedError: ErrEmptyMaxNumValidators,
 		},
-		/*{
-			TestName: "Empty wallet address of the genesis validator.",
-			InvalidOpts: &Options{
+		{
+			TestName: "Empty unbonding period of days",
+			InvalidCommand: GenerateGenesisCommand{
 				network: "test",
-				maxValidators: big.NewInt(3),
-				unbondingPeriod: big.NewInt(0),
+				maxNumValidators: "5",
+				unbondingPeriod: "",
 			},
 			ExpectedError: ErrEmptyUnbondingPeriod,
-		},*/
+		},
+		{
+			TestName: "Empty wallet address of genesis validator",
+			InvalidCommand: GenerateGenesisCommand{
+				network: "test",
+				maxNumValidators: "5",
+				unbondingPeriod: "5",
+				walletAddressGenesisValidator: "",
+			},
+			ExpectedError: ErrEmptyWalletAddressValidator,
+		},
+		{
+			TestName: "Invalid wallet address less than 20 bytes with Hex prefix",
+			InvalidCommand: GenerateGenesisCommand{
+				network: "test",
+				maxNumValidators: "5",
+				unbondingPeriod: "5",
+				walletAddressGenesisValidator: "0xe2ac86cbae1bbbb47d157516d334e70859a1be",
+			},
+			ExpectedError: ErrInvalidWalletAddressValidator,
+		},
+		{
+			TestName: "Invalid wallet address less than 20 bytes without Hex prefix",
+			InvalidCommand: GenerateGenesisCommand{
+				network: "test",
+				maxNumValidators: "5",
+				unbondingPeriod: "5",
+				walletAddressGenesisValidator: "e2ac86cbae1bbbb47d157516d334e70859a1be",
+			},
+			ExpectedError: ErrInvalidWalletAddressValidator,
+		},
 	}
 
 	for _, test := range tests {
@@ -39,7 +71,11 @@ func TestItFailsWhenRunningHandlerWithInvalidCommandValues(t *testing.T) {
 			handler := GenerateGenesisCommandHandler{}
 			err := handler.Handle(test.InvalidCommand)
 			if err != test.ExpectedError {
-				t.Fatalf("Invalid options did not return error. Expected error: %s", test.ExpectedError.Error())
+				t.Fatalf(
+					"Invalid options did not return error. Expected error: %s, received error: %s",
+					test.ExpectedError.Error(),
+					err.Error(),
+				)
 			}
 		})
 	}
