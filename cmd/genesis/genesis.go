90d48af0f6e1d7a57bf4a893afe040aa27d60abe
Jonathan Gimeno <jgimeno@gmail.com>
2018-04-04 20:41:09 +0200

    Update pflag in order to use cobra

--- cmd/genesis/genesis.go
+++ cmd/genesis/genesis.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"github.com/spf13/cobra"
+	"fmt"
+)
+
 func main() {
+	 cmd := &cobra.Command{
+		Use:   "hugo",
+		Short: "Hugo is a very fast static site generator",
+		Long: `A Fast and Flexible Static Site Generator built with
+                love by spf13 and friends in Go.
+                Complete documentation is available at http://hugo.spf13.com`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Holaj")
+		},
+	}
+
+	cmd.Execute()
 }
