37592973434fcae2813bcc5b38c45b594649834e
Jonathan Gimeno <jgimeno@gmail.com>
2018-04-04 20:05:37 +0200

    Add cobra command engine.

--- cmd/genesis/genesis.go
+++ cmd/genesis/genesis.go
@@ -1,23 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"github.com/prometheus/common/log"
-	"gopkg.in/urfave/cli.v1"
-	"os"
-)
-
 func main() {
-	app := cli.NewApp()
-	app.Name = "genesisgen"
-	app.Usage = "Generates a genesis file."
-
-	app.Action = func(c *cli.Context) {
-		fmt.Println("Hola!")
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
