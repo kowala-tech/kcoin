bd5fc25177490419c8a24d062d8b9e16695f37c5
Jonathan Gimeno <jgimeno@gmail.com>
2018-04-05 06:09:18 +0200

    Now it accepts all params except prefundedAccounts.

--- cmd/genesis/genesis.go
+++ cmd/genesis/genesis.go
@@ -9,26 +9,16 @@ import (
 
 var (
 	FileConfig string
+	cmd *cobra.Command
 )
 
-func main() {
-	cmd := createCommand()
-
-	if err := cmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-
-func createCommand() *cobra.Command {
-	cmd := &cobra.Command{
+func init() {
+	cmd = &cobra.Command{
 		Use:   "genesis",
 		Short: "Generator of a genesis file.",
 		Long:  `Generate a genesis.json file based on a config file or parameters.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			if FileConfig == "" {
-				fmt.Println("Params usage.")
-			} else {
+			if FileConfig != "" {
 				viper.SetConfigFile(FileConfig)
 
 				err := viper.ReadInConfig()
@@ -48,8 +38,6 @@ func createCommand() *cobra.Command {
 				extraData: viper.GetString("genesis.extraData"),
 			}
 
-			parsePrefundedAccounts(viper.Get("prefundedAccounts"))
-
 			handler := GenerateGenesisCommandHandler{w:os.Stdout}
 			err := handler.Handle(command)
 			if err != nil {
@@ -60,13 +48,39 @@ func createCommand() *cobra.Command {
 	}
 
 	cmd.Flags().StringVarP(&FileConfig, "config", "c", "", "Use to load configuration from config file.")
+	cmd.Flags().StringP("network", "n", "", "The network to use, test or main")
+	viper.BindPFlag("genesis.network", cmd.Flags().Lookup("network"))
+	cmd.Flags().StringP("maxNumValidators", "v", "", "The network to use, test or main")
+	viper.BindPFlag("genesis.maxNumValidators", cmd.Flags().Lookup("maxNumValidators"))
+	cmd.Flags().StringP("unbondingPeriod", "p", "", "The network to use, test or main")
+	viper.BindPFlag("genesis.unbondingPeriod", cmd.Flags().Lookup("unbondingPeriod"))
+	cmd.Flags().StringP("walletAddressGenesisValidator", "g", "", "The network to use, test or main")
+	viper.BindPFlag("genesis.walletAddressGenesisValidator", cmd.Flags().Lookup("walletAddressGenesisValidator"))
+	cmd.Flags().StringP("consensusEngine", "e", "", "The network to use, test or main")
+	viper.BindPFlag("genesis.consensusEngine", cmd.Flags().Lookup("consensusEngine"))
+	cmd.Flags().StringP("smartContractsOwner", "s", "", "The network to use, test or main")
+	viper.BindPFlag("genesis.smartContractsOwner", cmd.Flags().Lookup("smartContractsOwner"))
+	cmd.Flags().StringP("extraData", "d", "", "The network to use, test or main")
+	viper.BindPFlag("genesis.extraData", cmd.Flags().Lookup("extraData"))
 
-	return cmd
+
 }
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func parsePrefundedAccounts(accounts interface{}) []PrefundedAccount {
 	prefundedAccounts := make([]PrefundedAccount, 0)
 
-	accountArray := accounts.([]interface{})
+	accountArray, ok := accounts.([]interface{})
+	if !ok {
+		return []PrefundedAccount{}
+	}
+
 	for _, v := range accountArray {
 		val := v.(map[string]interface{})
 
