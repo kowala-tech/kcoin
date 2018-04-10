2b3ee78cb2468f63ace120ae70b6b55026ada95d
Evgeny Danienko <6655321@bk.ru>
2018-04-10 13:49:24 +0300

    rebase kcoinclient package

--- kcoinclient/signer.go
+++ kcoinclient/signer.go
@@ -28,24 +28,16 @@ func (s *senderFromServer) Equal(other types.Signer) bool {
 	return ok && os.blockhash == s.blockhash
 }
 
-func (s *senderFromServer) Sender(hash common.Hash, R, S, V *big.Int) (common.Address, error) {
+func (s *senderFromServer) Sender(tx *types.Transaction) (common.Address, error) {
 	if s.blockhash == (common.Hash{}) {
 		return common.Address{}, errNotCached
 	}
 	return s.addr, nil
 }
 
-func (s *senderFromServer) ChainID() *big.Int {
-	return nil
-}
-
-func (s *senderFromServer) ChainIDMul() *big.Int {
-	return nil
-}
-
 func (s *senderFromServer) Hash(tx *types.Transaction) common.Hash {
 	panic("can't sign with senderFromServer")
 }
-func (s *senderFromServer) SignatureValues(sig []byte) (R, S, V *big.Int, err error) {
+func (s *senderFromServer) SignatureValues(tx *types.Transaction, sig []byte) (R, S, V *big.Int, err error) {
 	panic("can't sign with senderFromServer")
 }
