e134cd49c3fab593ca9bf1706a352608232679ff
Evgeny Danienko <6655321@bk.ru>
2018-05-29 22:02:52 +0300

    vmTracer concurrent map access

--- kcoin/genesis/tracer.go
+++ kcoin/genesis/tracer.go
@@ -1,6 +1,7 @@
 package genesis
 
 import (
+	"sync"
 	"time"
 
 	"github.com/kowala-tech/kcoin/common"
@@ -9,6 +10,7 @@ import (
 
 type vmTracer struct {
 	data map[common.Address]map[common.Hash]common.Hash
+	sync.RWMutex
 }
 
 func newVmTracer() *vmTracer {
@@ -23,14 +25,39 @@ func (vmt *vmTracer) CaptureState(env *vm.EVM, pc uint64, op vm.OpCode, gas, cos
 	}
 	if op == vm.SSTORE {
 		s := stack.Data()
-		addrStorage, ok := vmt.data[contract.Address()]
+
+		addrStorage, ok := vmt.getAddrStorage(contract.Address())
 		if !ok {
 			addrStorage = make(map[common.Hash]common.Hash, 1024)
-			vmt.data[contract.Address()] = addrStorage
+			vmt.setAddrStorage(contract.Address(), addrStorage)
 		}
 		addrStorage[common.BigToHash(s[len(s)-1])] = common.BigToHash(s[len(s)-2])
 	}
 	return nil
+}
+
+func (vmt *vmTracer) getAddrStorage(contractAddress common.Address) (map[common.Hash]common.Hash, bool) {
+	vmt.RLock()
+	defer vmt.RUnlock()
+
+	addrStorage, ok := vmt.data[contractAddress]
+	return addrStorage, ok
+}
+
+func (vmt *vmTracer) setAddrStorage(contractAddress common.Address, addrStorage map[common.Hash]common.Hash) {
+	vmt.Lock()
+	defer vmt.Unlock()
+
+	vmt.data[contractAddress] = addrStorage
+	return
+}
+
+func (vmt *vmTracer) setAddrStorageData(key, value common.Hash, addrStorage map[common.Hash]common.Hash) {
+	vmt.Lock()
+	defer vmt.Unlock()
+
+	addrStorage[key] = value
+	return
 }
 
 func (vmt *vmTracer) CaptureEnd(output []byte, gasUsed uint64, t time.Duration, err error) error {
