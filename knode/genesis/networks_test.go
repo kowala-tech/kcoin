c18a7b26c184b054c181cfde213a7671c8fab384
Jose Pinto <bluecor@gmail.com>
2018-06-14 13:57:16 +0100

    added tests for network config

---
+++ knode/genesis/networks_test.go
@@ -0,0 +1,61 @@
+package genesis
+
+import (
+	"testing"
+	"flag"
+	"path/filepath"
+	"io/ioutil"
+	"bytes"
+	"bufio"
+	"github.com/stretchr/testify/assert"
+	"encoding/json"
+	"fmt"
+)
+
+var update = flag.Bool("update", false, "update .golden files")
+
+// TestNetworkConfigs ensure that who changes the network gets a failing
+// test as this configs are not supposed to change
+// golden files exists to ensure that the config code matches the committed config
+// if we need to change network config then we should generate new golden files with
+// go test . --update
+func TestNetworkConfigs(t *testing.T) {
+	for kcoin, network := range Networks {
+		t.Run(fmt.Sprintf("Config for kcoin %s hasn't changed", kcoin), func(t *testing.T) {
+			for name, config := range network {
+				t.Run(fmt.Sprintf("Config for network %s hasn't changed", name), func(t *testing.T) {
+					jsonConfig := jsonEncodeGenesisConfig(config, t)
+					filename := filepath.Join("testfiles", kcoin+"-"+name+".json.golden")
+					if *update {
+						updateGolden(t, filename, jsonConfig)
+					}
+
+					g, err := ioutil.ReadFile(filename)
+					if err != nil {
+						t.Fatalf("failed reading .golden: %s", err)
+					}
+
+					assert.Equal(t, jsonConfig.Bytes(), g)
+				})
+			}
+		})
+	}
+}
+
+func updateGolden(t *testing.T, filename string, jsonConfig bytes.Buffer) {
+	t.Logf("updated golden file for %s", filename)
+	if err := ioutil.WriteFile(filename, jsonConfig.Bytes(), 0644); err != nil {
+		t.Fatalf("failed to update golden file: %s", err)
+	}
+}
+
+func jsonEncodeGenesisConfig(config Options, t *testing.T) bytes.Buffer {
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	err := json.NewEncoder(w).Encode(config)
+	if err != nil {
+		t.Fatalf("failed encoding config to json: %s", err)
+	}
+	w.Flush()
+	return b
+}
