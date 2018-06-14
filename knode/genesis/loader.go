42e5c05337c1b10cc7790f14a934d23365cf31c4
Jonathan Gimeno <jgimeno@gmail.com>
2018-06-14 11:23:29 +0200

    Refactor loader.

--- knode/genesis/loader.go
+++ knode/genesis/loader.go
@@ -15,39 +15,46 @@ import (
 //the other params and loads from a file.
 func NetworkGenesisBlock(filePath, kcoin, networkType string) (*core.Genesis, error) {
 	if len(filePath) == 0 {
-
-		kcoinOptions, ok := Networks[kcoin]
-		if !ok {
-			return nil, errors.New("invalid kcoin")
-		}
-
-		genesisOpts, ok := kcoinOptions[networkType]
-		if !ok {
-			return nil, errors.New("invalid network options")
-		}
-
-		return Generate(genesisOpts)
+		return loadFromConfig(kcoin, networkType)
 	} else { // Extract from file.
-		file, err := os.Open(filePath)
-		if err != nil {
-			return nil, errors.New(
-				fmt.Sprintf(
-					"Failed to read genesis file: %v",
-					err,
-				),
-			)
-		}
-		defer file.Close()
-
-		var genesis *core.Genesis
-		if err := json.NewDecoder(file).Decode(genesis); err != nil {
-			return nil, errors.New(
-				fmt.Sprintf(
-					"invalid genesis file: %v",
-					err,
-				),
-			)
-		}
-		return genesis, nil
+		return loadFromFile(filePath)
 	}
+}
+
+func loadFromFile(filePath string) (*core.Genesis, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, errors.New(
+			fmt.Sprintf(
+				"Failed to read genesis file: %v",
+				err,
+			),
+		)
+	}
+	defer file.Close()
+
+	var genesis *core.Genesis
+	if err := json.NewDecoder(file).Decode(genesis); err != nil {
+		return nil, errors.New(
+			fmt.Sprintf(
+				"invalid genesis file: %v",
+				err,
+			),
+		)
+	}
+	return genesis, nil
+}
+
+func loadFromConfig(kcoin, networkType string) (*core.Genesis, error) {
+	kcoinOptions, ok := Networks[kcoin]
+	if !ok {
+		return nil, errors.New("invalid kcoin")
+	}
+
+	genesisOpts, ok := kcoinOptions[networkType]
+	if !ok {
+		return nil, errors.New("invalid network options")
+	}
+
+	return Generate(genesisOpts)
 }
