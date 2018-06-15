0c43d265b9df7240d4e1ff1dc96f4693d9fb607f
Albert Callarisa <albert@acroca.com>
2018-06-15 15:14:04 +0200

    Accept a `--genesis-path` arg into the runner to load a specific genesis

--- release/kcoin.sh
+++ release/kcoin.sh
@@ -4,6 +4,7 @@ set -e
 TESTNET=false;
 NEW_ACC=false;
 NEW_ACC_PASS="";
+GENESIS_PATH="";
 
 command="";
 
@@ -23,6 +24,9 @@ do
 	  "--new-account-password="*)
 		NEW_ACC_PASS="${opt#*=}"
 		;;
+	  "--genesis-path="*)
+		GENESIS_PATH="${opt#*=}"
+		;;
 	  *)
 		command="$command$opt " # make sure this passed to the binary
 		;;
@@ -32,8 +36,8 @@ done
 cd /kcoin
 
 case $TESTNET in
-	(true)  ./kcoin init --testnet;;
-	(false) ./kcoin init;;
+	(true)  ./kcoin init --testnet "$GENESIS_PATH";;
+	(false) ./kcoin init "$GENESIS_PATH";;
 esac
 
 case $NEW_ACC in
