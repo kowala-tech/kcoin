73aac2b289c09e674108d2cd270f8fa1aafc3ae8
Jonathan Gimeno <jgimeno@gmail.com>
2018-07-04 04:21:10 +0200

    Persistence worker

--- notifications/cmd/transactions_persistence/main.go
+++ notifications/cmd/transactions_persistence/main.go
@@ -2,25 +2,19 @@ package main
 
 import (
 	"os"
-	"strconv"
-
-	"github.com/sirupsen/logrus"
+		"github.com/sirupsen/logrus"
 
 	"github.com/go-redis/redis"
 	"github.com/yourheropaul/inj"
 
-	"github.com/kowala-tech/kcoin/notifications/blockchain"
-	"github.com/kowala-tech/kcoin/notifications/core"
+		"github.com/kowala-tech/kcoin/notifications/core"
 	"github.com/kowala-tech/kcoin/notifications/environment"
-	"github.com/kowala-tech/kcoin/notifications/keyvalue"
-	"github.com/kowala-tech/kcoin/notifications/persistence"
+		"github.com/kowala-tech/kcoin/notifications/persistence"
 )
 
 func main() {
 	envReader := environment.NewReaderOs()
-	rpcURI := envReader.Read("TESTNET_RPC_ADDR")
 	redisAddr := envReader.Read("REDIS_ADDR")
-	pollingStr := envReader.Read("POLLING_INTERVAL")
 	logLevelRaw := envReader.Read("LOG_LEVEL")
 	if logLevelRaw == "" {
 		logLevelRaw = "info"
@@ -34,17 +28,6 @@ func main() {
 	logger := logrus.New()
 	logger.SetLevel(logLevel)
 	logger.Out = os.Stdout
-
-	var pollingSeconds int
-	if pollingStr == "" {
-		pollingSeconds = 5
-	} else {
-		parsed, err := strconv.Atoi(pollingStr)
-		if err != nil {
-			panic(err)
-		}
-		pollingSeconds = parsed
-	}
 
 	redisClient := redis.NewClient(&redis.Options{
 		Addr:     redisAddr,
@@ -62,8 +45,6 @@ func main() {
 	g := inj.NewGraph()
 	g.Provide(
 		worker,
-		blockchain.NewKcoin(rpcURI, pollingSeconds, logrus.NewEntry(logger)),
-		keyvalue.WrapKeyValue(keyvalue.NewRedisKeyValue(redisClient), "db_sync_latest_block"),
 		persistence.NewRedisPersistence(redisClient),
 	)
 
