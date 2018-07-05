6b18fafc4943b91d1b819791964cece56884bb9b
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-07-06 00:35:07 +0100

    contracts/bindings/oracle: removed extra error

--- client/contracts/bindings/oracle/oracle.go
+++ client/contracts/bindings/oracle/oracle.go
@@ -1,20 +1,16 @@
 package oracle
 
 import (
-	"errors"
 	"math/big"
 
 	"github.com/kowala-tech/kcoin/client/accounts/abi/bind"
 	"github.com/kowala-tech/kcoin/client/common"
+	"github.com/kowala-tech/kcoin/client/contracts/bindings"
 	"github.com/kowala-tech/kcoin/client/params"
 )
 
 //go:generate solc --allow-paths ., --abi --bin --overwrite -o build github.com/kowala-tech/kcoin/client/contracts/=../../truffle/contracts openzeppelin-solidity/=../../truffle/node_modules/openzeppelin-solidity/  ../../truffle/contracts/oracle/OracleMgr.sol
 //go:generate ../../../build/bin/abigen -abi build/OracleMgr.abi -bin build/OracleMgr.bin -pkg oracle -type OracleMgr -out ./gen_manager.go
-
-var (
-	errNoAddress = errors.New("there isn't an address for the provided chain ID")
-)
 
 var mapOracleMgrToAddr = map[uint64]common.Address{
 	params.TestnetChainConfig.ChainID.Uint64(): common.HexToAddress("0x4C55B59340FF1398d6aaE362A140D6e93855D4A5"),
@@ -29,7 +25,7 @@ type Manager interface {
 func Binding(contractBackend bind.ContractBackend, chainID *big.Int) (*OracleMgrSession, error) {
 	addr, ok := mapOracleMgrToAddr[chainID.Uint64()]
 	if !ok {
-		return nil, errNoAddress
+		return nil, bindings.ErrNoAddress
 	}
 
 	mgr, err := NewOracleMgr(addr, contractBackend)
