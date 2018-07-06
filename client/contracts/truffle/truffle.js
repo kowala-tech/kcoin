a098aea7c45eaaa0783055e7b3e9c3837f106920
Adrian Hetman <adrianhetman@gmail.com>
2018-07-06 16:49:26 +0200

    added ENS contracts

--- client/contracts/truffle/truffle.js
+++ client/contracts/truffle/truffle.js
@@ -5,6 +5,11 @@ module.exports = {
       port: 8555,
       network_id: '*', // Match any network id
     },
+    dev: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
   },
   solc: {
     optimizer: {
