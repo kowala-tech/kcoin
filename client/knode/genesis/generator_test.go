bbba12affaf45c1c3862217eea585e3470bfc8c7
Jose Pinto <bluecor@gmail.com>
2018-07-19 09:34:31 +0100

    test for genesis block generated

--- client/knode/genesis/generator_test.go
+++ client/knode/genesis/generator_test.go
@@ -3,6 +3,7 @@ package genesis
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenerateIsDeterministic(t *testing.T) {
@@ -10,9 +11,11 @@ func TestGenerateIsDeterministic(t *testing.T) {
 
 	generatedGenesis, err := Generate(options)
 	assert.NoError(t, err)
+	require.NotNil(t, generatedGenesis)
 
 	generatedGenesisTwo, err := Generate(options)
 	assert.NoError(t, err)
+	require.NotNil(t, generatedGenesisTwo)
 
 	assert.Equal(t, getHashFromGenesisBlock(generatedGenesis), getHashFromGenesisBlock(generatedGenesisTwo))
 }
@@ -21,10 +24,12 @@ func TestGenerateIsDeterministicHasDifferentHash(t *testing.T) {
 	options := Networks["kusd"][MainNetwork]
 	generatedGenesis, err := Generate(options)
 	assert.NoError(t, err)
+	require.NotNil(t, generatedGenesis)
 
 	options.ExtraData = "Something different in this config"
 	generatedGenesisTwo, err := Generate(options)
 	assert.NoError(t, err)
+	require.NotNil(t, generatedGenesisTwo)
 
 	assert.NotEqual(t, getHashFromGenesisBlock(generatedGenesis), getHashFromGenesisBlock(generatedGenesisTwo))
 }
