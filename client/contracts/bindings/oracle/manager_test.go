7bf4c5d3b26ef044cee5721d521f9b63fc86c4af
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-08-05 19:48:06 +0100

    contracts/oracle: refactored tests

--- client/contracts/bindings/oracle/manager_test.go
+++ client/contracts/bindings/oracle/manager_test.go
@@ -2,6 +2,7 @@ package oracle_test
 
 import (
 	"math/big"
+	"strings"
 	"testing"
 
 	"github.com/kowala-tech/kcoin/client/accounts/abi/bind"
@@ -25,7 +26,8 @@ var (
 
 type OracleMgrSuite struct {
 	suite.Suite
-	backend *backends.SimulatedBackend
+	backend   *backends.SimulatedBackend
+	oracleMgr *oracle.OracleMgr
 }
 
 func TestOracleMgrSuite(t *testing.T) {
@@ -45,6 +47,40 @@ func (suite *OracleMgrSuite) BeforeTest(suiteName, testName string) {
 	})
 	req.NotNil(backend)
 	suite.backend = backend
+
+	if strings.Contains(testName, "TestDeploy") {
+		return
+	}
+
+	mockSuperNode := true
+	maxNumOracles := 50
+	switch {
+	case strings.Contains(testName, "_NotSuperNode"):
+		mockSuperNode = false
+		fallthrough
+	case strings.Contains(testName, "_Full"):
+		maxNumOracles = 1
+	}
+
+	transactOpts := bind.NewKeyedTransactor(owner)
+
+	// deploy consensus
+	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSuperNode)
+	req.NoError(err)
+	req.NotZero(mockAddr)
+
+	suite.backend.Commit()
+
+	// deploy oracle mgr contract
+	syncFreq := big.NewInt(900)
+	updatePeriod := big.NewInt(50)
+	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, big.NewInt(int64(maxNumOracles)), syncFreq, updatePeriod, mockAddr)
+	req.NoError(err)
+	req.NotNil(oracleMgrContract)
+	suite.oracleMgr = oracleMgrContract
+
+	suite.backend.Commit()
+
 }
 
 func (suite *OracleMgrSuite) TestDeploy() {
@@ -154,270 +190,114 @@ func (suite *OracleMgrSuite) TestRegisterOracle_WhenPaused() {
 
 	transactOpts := bind.NewKeyedTransactor(owner)
 
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// pause service
-	oracleMgrContract.Pause(transactOpts)
+	suite.oracleMgr.Pause(transactOpts)
 
 	suite.backend.Commit()
 
 	// register oracle must fail
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.Error(err, "service is paused")
 }
 
 func (suite *OracleMgrSuite) TestRegisterOracle_NotPaused_Duplicate() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register an oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
 	// register the same oracle again
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err = suite.oracleMgr.RegisterOracle(registerOpts)
 	req.Error(err, "duplicate registration")
 }
 
 func (suite *OracleMgrSuite) TestRegisterOracle_NotPaused_NewCandidate_NotSuperNode() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := false // super node set to false
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(1)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register an oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.Error(err, "user is not a super node")
 }
 
 func (suite *OracleMgrSuite) TestRegisterOracle_NotPaused_NewCandidate_SuperNode_Full() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(1)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register a new oracle to match the max number of oracles
 	registerOpts := bind.NewKeyedTransactor(owner)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 
 	// register a new oracle
 	registerOpts = bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err = suite.oracleMgr.RegisterOracle(registerOpts)
 	req.Error(err, "no positions available")
 }
 
 func (suite *OracleMgrSuite) TestRegisterOracle_NotPaused_NewCandidate_SuperNode_NotFull() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(1)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register a new oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 }
 
 func (suite *OracleMgrSuite) TestDeregisterOracle_WhenPaused() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
 	// pause service
-	oracleMgrContract.Pause(transactOpts)
+	pauseOpts := bind.NewKeyedTransactor(owner)
+	suite.oracleMgr.Pause(pauseOpts)
 
 	suite.backend.Commit()
 
 	// deregister oracle
 	deregisterOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.DeregisterOracle(deregisterOpts)
+	_, err = suite.oracleMgr.DeregisterOracle(deregisterOpts)
 	req.Error(err, "service is paused")
 }
 
 func (suite *OracleMgrSuite) TestDeregisterOracle_NotPaused_NotOracle() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// deregister oracle
 	deregisterOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.DeregisterOracle(deregisterOpts)
+	_, err := suite.oracleMgr.DeregisterOracle(deregisterOpts)
 	req.Error(err, "the user is not an oracle")
 }
 
 func (suite *OracleMgrSuite) TestDeregisterOracle_NotPaused_Oracle() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
 	// deregister oracle
 	deregisterOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.DeregisterOracle(deregisterOpts)
+	_, err = suite.oracleMgr.DeregisterOracle(deregisterOpts)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
-	count, err := oracleMgrContract.GetOracleCount(&bind.CallOpts{})
+	// oracle count must be zero
+	count, err := suite.oracleMgr.GetOracleCount(&bind.CallOpts{})
 	req.NoError(err)
 	req.Zero(count.Uint64())
 }
@@ -425,146 +305,67 @@ func (suite *OracleMgrSuite) TestDeregisterOracle_NotPaused_Oracle() {
 func (suite *OracleMgrSuite) TestSubmitPrice_WhenPaused() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
 	// pause service
-	oracleMgrContract.Pause(transactOpts)
+	pauseOpts := bind.NewKeyedTransactor(owner)
+	suite.oracleMgr.Pause(pauseOpts)
 
 	suite.backend.Commit()
 
 	priceOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.SubmitPrice(priceOpts, common.Big2)
+	_, err = suite.oracleMgr.SubmitPrice(priceOpts, common.Big2)
 	req.Error(err, "service is paused")
 }
 
 func (suite *OracleMgrSuite) TestSubmitPrice_NotPaused_NotOracle() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	priceOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.SubmitPrice(priceOpts, common.Big2)
+	_, err := suite.oracleMgr.SubmitPrice(priceOpts, common.Big2)
 	req.Error(err, "the user is not an oracle")
 }
 
 func (suite *OracleMgrSuite) TestSubmitPrice_NotPaused_Oracle() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
 	// submit price
 	priceOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.SubmitPrice(priceOpts, common.Big2)
+	_, err = suite.oracleMgr.SubmitPrice(priceOpts, common.Big2)
 	req.NoError(err)
 }
 
 func (suite *OracleMgrSuite) TestSubmitPrice_NotPaused_Oracle_SubmitPriceTwiceSameRound() {
 	req := suite.Require()
 
-	transactOpts := bind.NewKeyedTransactor(owner)
-
-	// deploy consensus
-	mockSupernode := true // super node must be true to register the oracle
-	mockAddr, _, _, err := testfiles.DeployConsensusMock(transactOpts, suite.backend, mockSupernode)
-	req.NoError(err)
-	req.NotZero(mockAddr)
-
-	suite.backend.Commit()
-
-	// deploy oracle mgr contract
-	maxNumOracles := big.NewInt(50)
-	syncFreq := big.NewInt(900)
-	updatePeriod := big.NewInt(50)
-	_, _, oracleMgrContract, err := oracle.DeployOracleMgr(transactOpts, suite.backend, maxNumOracles, syncFreq, updatePeriod, mockAddr)
-	req.NoError(err)
-	req.NotNil(oracleMgrContract)
-
-	suite.backend.Commit()
-
 	// register oracle
 	registerOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.RegisterOracle(registerOpts)
+	_, err := suite.oracleMgr.RegisterOracle(registerOpts)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
 	// submit price
 	priceOpts := bind.NewKeyedTransactor(user)
-	_, err = oracleMgrContract.SubmitPrice(priceOpts, common.Big2)
+	_, err = suite.oracleMgr.SubmitPrice(priceOpts, common.Big2)
 	req.NoError(err)
 
 	suite.backend.Commit()
 
 	// submit price
-	_, err = oracleMgrContract.SubmitPrice(priceOpts, common.Big2)
+	_, err = suite.oracleMgr.SubmitPrice(priceOpts, common.Big2)
 	req.Error(err, "cannot submit a price twice in the same round")
 }
 
