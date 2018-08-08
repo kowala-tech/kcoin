3c698c48a3557000b3cf053633351bab2a4c2f5a
Jonathan Gimeno <jgimeno@gmail.com>
2018-08-08 22:04:14 +0200

    Update proxy resolver addr.

--- client/knode/genesis/contracts.go
+++ client/knode/genesis/contracts.go
@@ -21,7 +21,7 @@ import (
 var ProxyFactoryAddr = "0x1582aEd4A8156325e28ef9eF075Da1E1D44AA56E"
 var ProxyKNSRegistryAddr = "0xDfA58b86D285cF07E15ceEc0BaAb89e1D26305d7"
 var ProxyRegistrarAddr = "0x927C9AF9CF36a9d43d3160576e750612bc9e63e9"
-var ProxyResolverAddr = "0x4C55B59340FF1398d6aaE362A140D6e93855D4A5"
+var ProxyResolverAddr = "0x577baB773e23327B0Ab7081E2a1ffcd186514d75"
 
 type contract struct {
 	name       string
