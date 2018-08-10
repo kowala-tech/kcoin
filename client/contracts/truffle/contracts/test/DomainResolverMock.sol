600a3d50edb86107e8cdc3ac28f5fc92e992b15b
Adrian Hetman <adrianhetman@gmail.com>
2018-08-10 16:58:24 +0200

    Fix for the DomainResolverMock

--- client/contracts/truffle/contracts/test/DomainResolverMock.sol
+++ client/contracts/truffle/contracts/test/DomainResolverMock.sol
@@ -4,12 +4,14 @@ pragma solidity 0.4.24;
  * Mock contract for PublicResolver.
  */
 contract DomainResolver {
-    /**
-     * Returns the address associated with an KNS node.
-     * @param node The KNS node to query.
-     * @return The associated address.
-     */
+
+    address domainAddr;
+
+    constructor(address _domainAddr){
+      domainAddr = _domainAddr;
+    }
+
     function addr(bytes32 node) public view returns (address){
-      return 0x1234;
+      return domainAddr;
     }
 }
