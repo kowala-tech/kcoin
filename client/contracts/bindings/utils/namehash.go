efc95e0e8e768a729d5066358a3a4acd318ffcd8
Jonathan Gimeno <jgimeno@gmail.com>
2018-08-14 20:17:57 +0200

    Add generated files.

--- client/contracts/bindings/utils/namehash.go
+++ client/contracts/bindings/utils/namehash.go
@@ -1,6 +1,6 @@
 package utils
 
 //go:generate solc --allow-paths ., --abi --bin --overwrite -o build ../../truffle/contracts/utils/Strings.sol
-//go:generate ../../../build/bin/abigen -abi build/strings.abi -bin build/strings.bin -pkg utils -type Strings -out ./gen_strings.go
+//go:generate ../../../build/bin/abigen -abi build/strings.abi -bin build/strings.bin -pkg utils -type strings -out ./gen_strings.go
 //go:generate solc --allow-paths ., --abi --bin --libraries Strings:0x7d55e20244765F0Dda1aC0b91BA2BA1c5AA9D270 --overwrite -o build ../../truffle/contracts/utils/NameHash.sol
 //go:generate ../../../build/bin/abigen -abi build/NameHash.abi -bin build/NameHash.bin -pkg utils -type NameHash -out ./gen_namehash.go
