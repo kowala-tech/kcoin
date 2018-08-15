7f89e6bbe2c077cd03d7089be29ab6c3a2185ddc
Jonathan Gimeno <jgimeno@gmail.com>
2018-08-15 18:38:38 +0200

    Add methods from merge.

--- client/contracts/bindings/consensus/consensus.go
+++ client/contracts/bindings/consensus/consensus.go
@@ -22,7 +22,7 @@ import (
 	"github.com/kowala-tech/kcoin/client/params"
 )
 
-//go:generate solc --allow-paths ., --abi --bin --overwrite --libraries NameHash:0x66DA4aC1767B04B0d99bC94CCaD6EEF8dA63Ae96 -o build github.com/kowala-tech/kcoin/client/contracts/=../../truffle/contracts openzeppelin-solidity/=../../truffle/node_modules/openzeppelin-solidity/  ../../truffle/contracts/consensus/mgr/ValidatorMgr.sol
+//go:generate solc --allow-paths ., --abi --bin --overwrite -o build github.com/kowala-tech/kcoin/client/contracts/=../../truffle/contracts openzeppelin-solidity/=../../truffle/node_modules/openzeppelin-solidity/  ../../truffle/contracts/consensus/mgr/ValidatorMgr.sol
 //go:generate ../../../build/bin/abigen -abi build/ValidatorMgr.abi -bin build/ValidatorMgr.bin -pkg consensus -type ValidatorMgr -out ./gen_manager.go
 //go:generate solc --allow-paths ., --abi --bin --overwrite -o build github.com/kowala-tech/kcoin/client/contracts/=../../truffle/contracts openzeppelin-solidity/=../../truffle/node_modules/openzeppelin-solidity/ ../../truffle/contracts/consensus/token/MiningToken.sol
 //go:generate ../../../build/bin/abigen -abi build/MiningToken.abi -bin build/MiningToken.bin -pkg consensus -type MiningToken -out ./gen_mtoken.go
@@ -55,7 +55,9 @@ type Consensus interface {
 }
 
 type Minter interface {
-	Mint(opts *accounts.TransactOpts, to common.Address) (common.Hash, error)
+	MultiSigWalletContract() *ownership.MultiSigWallet
+	Mint(opts *accounts.TransactOpts, to common.Address, value *big.Int) (common.Hash, error)
+	Confirm(opts *accounts.TransactOpts, transactionID *big.Int) (common.Hash, error)
 }
 
 type mUSD struct {
@@ -96,15 +98,18 @@ func (tkn *mUSD) Mint(opts *accounts.TransactOpts, to common.Address, value *big
 }
 
 func toBind(opts *accounts.TransactOpts) *bind.TransactOpts {
-	return &bind.TransactOpts{
+	bindOpts := &bind.TransactOpts{
 		From:     opts.From,
 		Nonce:    opts.Nonce,
-		Signer:   bind.SignerFn(opts.Signer),
 		Value:    opts.Value,
 		GasPrice: opts.GasPrice,
-		GasLimit: opts.GasLimit.Uint64(),
 		Context:  opts.Context,
+		Signer:   bind.SignerFn(opts.Signer),
 	}
+	if opts.GasLimit != nil {
+		bindOpts.GasLimit = opts.GasLimit.Uint64()
+	}
+	return bindOpts
 }
 
 func (tkn *mUSD) BalanceOf(target common.Address) (*big.Int, error) {
@@ -122,6 +127,7 @@ type consensus struct {
 	chainID         *big.Int
 	contractBackend bind.ContractBackend
 
+	mtokenAddr     common.Address
 	initMint       sync.Once
 	multiSigWallet *ownership.MultiSigWallet
 	oracle         *oracle.OracleMgr
@@ -314,7 +320,11 @@ func (consensus *consensus) MintInit() error {
 	return err
 }
 
-func (consensus *consensus) Mint(opts *accounts.TransactOpts, to common.Address) (common.Hash, error) {
+func (consensus *consensus) MultiSigWalletContract() *ownership.MultiSigWallet {
+	return consensus.multiSigWallet
+}
+
+func (consensus *consensus) Mint(opts *accounts.TransactOpts, to common.Address, value *big.Int) (common.Hash, error) {
 	if err := consensus.MintInit(); err != nil {
 		return common.Hash{}, err
 	}
@@ -324,12 +334,25 @@ func (consensus *consensus) Mint(opts *accounts.TransactOpts, to common.Address)
 		return common.Hash{}, err
 	}
 
-	mintParams, err := tokenABI.Pack("mint", to, opts.Value)
+	mintParams, err := tokenABI.Pack("mint", to, value)
 	if err != nil {
 		return common.Hash{}, err
 	}
 
-	tx, err := consensus.multiSigWallet.SubmitTransaction(toBind(opts), to, common.Big0, mintParams)
+	tx, err := consensus.multiSigWallet.SubmitTransaction(toBind(opts), consensus.mtokenAddr, common.Big0, mintParams)
+	if err != nil {
+		return common.Hash{}, err
+	}
+
+	return tx.Hash(), err
+}
+
+func (consensus *consensus) Confirm(opts *accounts.TransactOpts, transactionID *big.Int) (common.Hash, error) {
+	if err := consensus.MintInit(); err != nil {
+		return common.Hash{}, err
+	}
+
+	tx, err := consensus.multiSigWallet.ConfirmTransaction(toBind(opts), transactionID)
 	if err != nil {
 		return common.Hash{}, err
 	}
