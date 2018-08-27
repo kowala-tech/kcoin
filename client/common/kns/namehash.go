da8acb726d59f137ca34c7c47f37414a4c30873b
Jonathan Gimeno <jgimeno@gmail.com>
2018-08-27 18:10:38 +0200

    Fix import order.

--- client/common/kns/namehash.go
+++ client/common/kns/namehash.go
@@ -1,8 +1,9 @@
 package kns
 
 import (
-	"github.com/kowala-tech/kcoin/client/common"
 	"strings"
+
+	"github.com/kowala-tech/kcoin/client/common"
 	"github.com/kowala-tech/kcoin/client/crypto"
 )
 
@@ -12,7 +13,7 @@ func NameHash(name string) common.Hash {
 	if len(name) > 0 {
 		labels := strings.Split(name, ".")
 
-		for i := len(labels) - 1; i >= 0;  i--  {
+		for i := len(labels) - 1; i >= 0; i-- {
 			labelSha := crypto.Keccak256Hash([]byte(labels[i]))
 			node = crypto.Keccak256Hash(node.Bytes(), labelSha.Bytes())
 		}
