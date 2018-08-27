7c7fc74c673c7968cfec2f576a709d3c4a50b341
Jonathan Gimeno <jgimeno@gmail.com>
2018-08-27 18:16:53 +0200

    Change name hash examples to use kns typology.

--- client/common/kns/namehash_test.go
+++ client/common/kns/namehash_test.go
@@ -2,11 +2,12 @@ package kns
 
 import (
 	"testing"
-	"gotest.tools/assert"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNameHash(t *testing.T) {
-	tests := []struct{
+	tests := []struct {
 		Name     string
 		Expected string
 		Input    string
@@ -17,14 +18,14 @@ func TestNameHash(t *testing.T) {
 			Input:    "",
 		},
 		{
-			Name:     "eth",
-			Expected: "0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae",
-			Input:    "eth",
+			Name:     "kns",
+			Expected: "0xbc25ce339c62a23a50c9bdae4aba9cb6dab4cefd53d1501dcbf2eec2583d200e",
+			Input:    "kowala",
 		},
 		{
-			Name:     "foo.eth",
-			Expected: "0xde9b09fd7c5f901e23a3f19fecc54828e9c848539801e86591bd9801b019f84f",
-			Input:    "foo.eth",
+			Name:     "foo.kowala",
+			Expected: "0x8b44e9e27ccae58fe2d6023ac02a4a4ee0bdea3ad1374151da2c1f0e99e883cc",
+			Input:    "foo.kowala",
 		},
 	}
 
