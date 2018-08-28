9c402c5d3252b96f4730ec8b0c90127188f4800f
Adrian Hetman <adrianhetman@gmail.com>
2018-08-28 16:56:06 +0200

    Added final tests for MultiSig

--- client/contracts/truffle/.solcover.js
+++ client/contracts/truffle/.solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
   norpc: true,
   copyPackages: ['openzeppelin-solidity', 'truffle', 'eth-ens-namehash', 'zos-lib'],
-  skipFiles: ['utils', 'test', 'sysvars'],
+  skipFiles: ['utils', 'test'],
 };
