22e17f75f99a938dcf99e35157dabac268e43f11
Ricardo Geraldes <rgeraldes24@gmail.com>
2018-08-30 14:04:59 +0100

    contracts/stability: modified withdrawal conditions - reward

--- client/contracts/truffle/contracts/stability/Stability.sol
+++ client/contracts/truffle/contracts/stability/Stability.sol
@@ -11,12 +11,14 @@ contract Stability is Pausable {
     uint constant ONE = 1 ether;
     
     uint public minDeposit;
+    uint public initialReward;
     PriceProvider priceProvider;
     
     struct Subscription {
         uint index;
         bool hasSubscription;
         uint deposit;
+        uint reward;
     }
 
     mapping (address => Subscription) private subscriptionRegistry;
@@ -33,18 +35,15 @@ contract Stability is Pausable {
         _;
     }
 
-    modifier whenPriceGreaterEqualOne {
-        require(priceProvider.price() >= ONE);
-        _;
-    }
-
     /**
      * Constructor
      * @param _minDeposit minimum deposit required to subscribe to the service
+     * @param _initialReward is the initial reward for subscribing (useful for tests)
      * @param _priceProviderAddr address of system variables contract
      */
-    function Stability(uint _minDeposit, address _priceProviderAddr) public {
+    function Stability(uint _minDeposit, uint _initialReward, address _priceProviderAddr) public {
         minDeposit = _minDeposit;
+        initialReward = _initialReward;
         priceProvider = PriceProvider(_priceProviderAddr);
     }
 
@@ -67,6 +66,7 @@ contract Stability is Pausable {
         subs.index = subscriptions.push(msg.sender) - 1;
         subs.hasSubscription = true;
         subs.deposit = msg.value;
+        subs.reward = initialReward;
     }
 
      /**
@@ -85,9 +85,10 @@ contract Stability is Pausable {
     /**
      * @dev Unsubscribe the service
      */
-    function unsubscribe() public onlySubscriber whenPriceGreaterEqualOne {
+    function unsubscribe() public onlySubscriber {
         Subscription subs = subscriptionRegistry[msg.sender];
         uint rowToDelete = subs.index;
+        if (priceProvider.price() >= ONE) msg.sender.transfer(subs.reward);
         msg.sender.transfer(subs.deposit);
         delete subscriptionRegistry[msg.sender];
 
