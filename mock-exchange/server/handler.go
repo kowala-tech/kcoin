ad0a1d04dbda9f625095f021d2d4874479215a60
Jonathan Gimeno <jgimeno@gmail.com>
2018-09-17 19:10:56 +0200

    Add handler case that gets the request data. WIP

--- mock-exchange/server/handler.go
+++ mock-exchange/server/handler.go
@@ -1,10 +1,34 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func fetchDataHandler(w http.ResponseWriter, r *http.Request) {
+type FetchDataRequest struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
+type FetchDataHandler struct {
+}
+
+func (*FetchDataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+	defer r.Body.Close()
+
+	request := FetchDataRequest{}
+	err := json.NewDecoder(r.Body).Decode(&request)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Printf("Error %s", err)
+		return
+	}
+
+	fmt.Printf("%v", request)
 }
--- mock-exchange/server/handler_test.go
+++ mock-exchange/server/handler_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +16,25 @@ func TestFetchDataHandlerNeedsToBePost(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(fetchDataHandler)
+	handler := FetchDataHandler{}
 
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusMethodNotAllowed, rr.Code)
+}
+
+func TestWeFetchDataWeWantToDisplay(t *testing.T) {
+	requestData := strings.NewReader(`{"from":"USD","to":"BTC"}`)
+
+	req, err := http.NewRequest(http.MethodPost, "/fetch", requestData)
+	if err != nil {
+		t.Fatalf("Error creating request: %s", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := FetchDataHandler{}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK)
 }
