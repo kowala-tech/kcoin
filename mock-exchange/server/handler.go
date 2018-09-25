3292d238004c609d3d5890b9094adffa43ce0666
Jonathan Gimeno <jgimeno@gmail.com>
2018-09-25 11:18:43 +0200

    simplify logger.

--- mock-exchange/server/handler.go
+++ mock-exchange/server/handler.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/kowala-tech/kcoin/mock-exchange/app"
@@ -20,11 +19,13 @@ type FetchDataHandler struct {
 }
 
 func (h *FetchDataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logger := logrus.WithFields(logrus.Fields{
+		"call":   "fetch",
+		"method": r.Method,
+	})
+
 	if r.Method != http.MethodPost {
-		logrus.WithFields(logrus.Fields{
-			"call":   "fetch",
-			"method": r.Method,
-		}).Warn("invalid method called")
+		logger.Warn("invalid method called")
 
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -34,20 +35,14 @@ func (h *FetchDataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	request := app.Request{}
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"call":    "fetch",
-			"request": request,
-		}).Warn(fmt.Sprintf("error creating request: %s", err))
+		logger.WithError(err).WithField("request", request).Warn("error creating request")
 
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	h.cache.Set(app.CacheRequestKey, request, cache.NoExpiration)
-	logrus.WithFields(logrus.Fields{
-		"call":    "fetch",
-		"request": request,
-	}).Info("saved request in cache")
+	logger.WithField("request", request).Info("saved request in cache")
 }
 
 type GetRatesHandler struct {
@@ -56,11 +51,13 @@ type GetRatesHandler struct {
 }
 
 func (h *GetRatesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logger := logrus.WithFields(logrus.Fields{
+		"call":   "getRate",
+		"method": r.Method,
+	})
+
 	if r.Method != http.MethodGet {
-		logrus.WithFields(logrus.Fields{
-			"call":   "getRate",
-			"method": r.Method,
-		}).Warn("invalid method called")
+		logger.Warn("invalid method called")
 
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -68,48 +65,33 @@ func (h *GetRatesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	req, ok := h.cache.Get(app.CacheRequestKey)
 	if !ok {
-
 		jsonErrResp, err := json.Marshal(ErrorResponse{Error: "Please, call fetch before."})
 		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"call":  "getRate",
-				"error": err,
-			}).Warn("error creating error response")
+			logger.WithError(err).Warn("error creating error response")
 			return
 		}
 
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(jsonErrResp)
 
-		logrus.WithFields(logrus.Fields{
-			"call": "getRate",
-		}).Info("msg", "get rates api method was called before fetching mocked info")
+		logger.Info("msg", "get rates api method was called before fetching mocked info")
 		return
 	}
 
 	tResp, err := h.transformer.Transform(req.(app.Request))
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		jsonErrResp, err := json.Marshal(ErrorResponse{Error: "There was a problem getting data from request."})
-		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"call":  "getRate",
-				"error": err,
-			}).Warn("error creating error response")
+		jsonErrResp, errJson := json.Marshal(ErrorResponse{Error: "There was a problem getting data from request."})
+		if errJson != nil {
+			logger.WithError(err).Warn("error creating error response")
 			return
 		}
 
 		w.Write(jsonErrResp)
-		logrus.WithFields(logrus.Fields{
-			"call":  "getRate",
-			"error": err,
-		}).Warn("msg", "there was a problem when trying to decodify data from cache")
+		logger.WithError(err).Warn("msg", "there was a problem when trying to decodify data from cache")
 		return
 	}
 
 	w.Write([]byte(tResp))
-	logrus.WithFields(logrus.Fields{
-		"call": "getRate",
-		"data": tResp,
-	}).Info("msg", "request for data rates accomplished")
+	logger.WithField("response", tResp).Info("msg", "request for data rates accomplished")
 }
